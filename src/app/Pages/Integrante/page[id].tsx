46fe6e2e0eb6cfdb11566123ecb84b96eaae6fba
enzo-roch4 <144125363+enzo-roch4@users.noreply.github.com>
2024-06-07 20:23:18 -0300

    Ajustado

--- src/app/Pages/Integrante/page[id].tsx
+++ src/app/Pages/Integrante/page[id].tsx
@@ -6,5 +6,15 @@ export default function Integrante() {
   const { id } = router.query;
   const integrante = integrantesData.find((integrante) => integrante.id === id);
 
- 
+  if (!integrante) {
+    return <div>Integrante não encontrado</div>;
+  }
+
+  return (
+    <div>
+      <h1>{integrante.nome}</h1>
+      <p>RM: {integrante.rm} - Turma: {integrante.turma}</p>
+      <img src={integrante.foto} alt={`Foto de ${integrante.nome}`} />
+    </div>
+  );
 }
